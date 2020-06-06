37af1043a3aa14f3229a92231267f6e6351802ae
Nikita Daineko <dna.lopotros.dma@gmail.com>
2020-06-06 12:19:59 +0300

    Hw_3

---
+++ HW_3/Task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime birthDate;
+            while (!DateTime.TryParse(Console.ReadLine(), out birthDate))
+            {
+                Console.WriteLine("Введите дату в формате: {0:d}", new DateTime(2000, 1, 1));
+            }
+            DateTime nowDate = DateTime.Now;
+            int age = nowDate.Year - birthDate.Year;
+            if (birthDate > nowDate.AddYears(-age)) age--;
+            Console.WriteLine("Ваш возраст " + age + " года/лет");
+        }
+    }
+}
